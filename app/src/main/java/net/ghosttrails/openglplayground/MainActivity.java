7e1943126a79780ff9911e9fd8d39f4ad01071de
Andrew Reid <andrew.k.reid@gmail.com>
2016-09-17 12:11:42 +1000

    Initial commit

---
+++ app/src/main/java/net/ghosttrails/openglplayground/MainActivity.java
@@ -0,0 +1,72 @@
+package net.ghosttrails.openglplayground;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+        setContentView(R.layout.activity_main);
+
+        if (supportsEs2()) {
+            // Request an OpenGL ES 2.0 compatible context
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            // assign our renderer
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        setContentView(glSurfaceView);
+    }
+
+    boolean supportsEs2() {
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic")
+                  || Build.FINGERPRINT.startsWith("unknown")
+                  || Build.MODEL.contains("google_sdk")
+                  || Build.MODEL.contains("Emulator")
+                  || Build.MODEL.contains("Android SDK built for x86")));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+}
+
+
+
+
+
+
