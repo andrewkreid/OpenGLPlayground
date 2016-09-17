803e6e78d8e5515a8ab1b25611091c645ff966d6
Andrew Reid <andrew.k.reid@gmail.com>
2016-09-17 13:34:52 +1000

    To end of Chapter 2

--- app/src/main/java/net/ghosttrails/openglplayground/AirHockeyActivity.java
+++ app/src/main/java/net/ghosttrails/openglplayground/AirHockeyActivity.java
@@ -2,18 +2,14 @@ package net.ghosttrails.openglplayground;
 
 import android.os.Bundle;
 
-public class MainActivity extends OpenGLBaseActivity {
+/**
+ * Created by andrewr on 17/09/16.
+ */
+public class AirHockeyActivity extends OpenGLBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        renderer = new FirstOpenGLProjectRenderer();
+        renderer = new AirHockeyRenderer();
         super.onCreate(savedInstanceState);
     }
-
 }
-
-
-
-
-
-
---
+++ app/src/main/java/net/ghosttrails/openglplayground/AirHockeyRenderer.java
@@ -0,0 +1,63 @@
+package net.ghosttrails.openglplayground;
+
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by andrewr on 17/09/16.
+ */
+public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+
+    public AirHockeyRenderer() {
+        float[] tableVertices = {
+
+                // Triangle 1
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+
+                // Triangle 2
+                0f, 0f,
+                9f, 0f,
+                9f, 14f,
+
+                // Line 1
+                0f, 7f,
+                9f, 7f,
+
+                // Mallets
+                4.5f, 2f,
+                4.5f, 12f
+        };
+
+        vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+}
