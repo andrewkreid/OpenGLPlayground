e8469eb6808e8c0698155deb322db40d9d719b6a
Andrew Reid <andrew.k.reid@gmail.com>
2016-10-23 19:36:29 +1100

    7.2 texture utils.

---
+++ app/src/main/java/net/ghosttrails/openglplayground/util/TextureHelper.java
@@ -0,0 +1,79 @@
+package net.ghosttrails.openglplayground.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import static android.opengl.GLES20.GL_LINEAR;
+import static android.opengl.GLES20.GL_LINEAR_MIPMAP_LINEAR;
+import static android.opengl.GLES20.GL_TEXTURE_2D;
+import static android.opengl.GLES20.GL_TEXTURE_MAG_FILTER;
+import static android.opengl.GLES20.GL_TEXTURE_MIN_FILTER;
+import static android.opengl.GLES20.glBindTexture;
+import static android.opengl.GLES20.glDeleteTextures;
+import static android.opengl.GLES20.glGenTextures;
+import static android.opengl.GLES20.glGenerateMipmap;
+import static android.opengl.GLES20.glTexParameteri;
+import static android.opengl.GLUtils.texImage2D;
+
+/**
+ *
+ * Created by andrewr on 23/10/16.
+ */
+
+public class TextureHelper {
+
+    private static final String TAG = "TextureHelper";
+
+    /**
+     * Load a bitmap into OpenGL from a resource Id
+     * @param context the context
+     * @param resourceId a bitmap resource
+     * @return a texture object Id.
+     */
+    public static int loadTexture(Context context, int resourceId) {
+        final int[] textureObjectIds = new int[1];
+        glGenTextures(1, textureObjectIds, 0);
+
+        if (textureObjectIds[0] == 0) {
+            if (LoggerConfig.ON) {
+                Log.w(TAG, "Could not generate a new OpenGL texture object.");
+            }
+            return 0;
+        }
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(
+                context.getResources(), resourceId, options);
+
+        if (bitmap == null) {
+            if (LoggerConfig.ON) {
+                Log.w(TAG, "Resource ID " + resourceId + " could not be decoded");
+            }
+
+            glDeleteTextures(1, textureObjectIds, 0);
+            return 0;
+        }
+
+        // Future calls will apply to this texture...
+        glBindTexture(GL_TEXTURE_2D, textureObjectIds[0]);
+
+        // Trilinear mipmap on minification, bilinear interp on magnification.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // load the bitmap into OpenGL
+        texImage2D(GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
+        
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        // unbind
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        return textureObjectIds[0];
+    }
+}
