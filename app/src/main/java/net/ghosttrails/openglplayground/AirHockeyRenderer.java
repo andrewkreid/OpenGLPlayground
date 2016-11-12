28d6c6774b6224514fc9535911968156591e5bda
Andrew Reid <andrew.k.reid@gmail.com>
2016-11-12 19:18:25 +1100

    9.2

--- app/src/main/java/net/ghosttrails/openglplayground/AirHockeyRenderer.java
+++ app/src/main/java/net/ghosttrails/openglplayground/AirHockeyRenderer.java
@@ -9,6 +9,7 @@ import net.ghosttrails.openglplayground.objects.Puck;
 import net.ghosttrails.openglplayground.objects.Table;
 import net.ghosttrails.openglplayground.programs.ColorShaderProgram;
 import net.ghosttrails.openglplayground.programs.TextureShaderProgram;
+import net.ghosttrails.openglplayground.util.Geometry;
 import net.ghosttrails.openglplayground.util.MatrixHelper;
 import net.ghosttrails.openglplayground.util.TextureHelper;
 
@@ -19,11 +20,15 @@ import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.invertM;
 import static android.opengl.Matrix.multiplyMM;
+import static android.opengl.Matrix.multiplyMV;
 import static android.opengl.Matrix.rotateM;
 import static android.opengl.Matrix.setIdentityM;
 import static android.opengl.Matrix.setLookAtM;
 import static android.opengl.Matrix.translateM;
+import static net.ghosttrails.openglplayground.util.Geometry.Point;
+import static net.ghosttrails.openglplayground.util.Geometry.Ray;
 
 /**
  * AirHockey Renderer
@@ -38,6 +43,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private final float[] modelViewProjectionMatrix = new float[16];
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
+    private final float[] invertedViewProjectionMatrix = new float[16];
 
     private Puck puck;
     private Table table;
@@ -45,6 +51,9 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private TextureShaderProgram textureProgram;
     private ColorShaderProgram colorProgram;
+
+    private boolean malletPressed = false;
+    private Point blueMalletPosition;
 
     private int texture;
 
@@ -64,6 +73,8 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         colorProgram = new ColorShaderProgram(context);
 
         texture = TextureHelper.loadTexture(context, R.drawable.air_hockey_surface);
+
+        blueMalletPosition = new Point(0f, mallet.height / 2f, 0.4f);
     }
 
     @Override
@@ -83,6 +94,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glClear(GL_COLOR_BUFFER_BIT);
 
         multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        invertM(invertedViewProjectionMatrix, 0, viewProjectionMatrix, 0);
 
         positionTableInScene();
 
@@ -122,8 +134,52 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
     }
 
+    private Ray convertNormalized2DPointToRay(float normalizedX, float normalizedY) {
+        // We'll convert these normalized device coordinates into world-space coordinates. We'll
+        // pick a point on the near and far planes, and draw a line between them. To do this
+        // transform, we need to first multiply by the inverse matrix, and then we need to undo
+        // the perspective divide.
+        final float[] nearPointNdc = {normalizedX, normalizedY, -1, 1};
+        final float[] farPointNdc = {normalizedX, normalizedY, 1, 1};
+
+        final float[] nearPointWorld = new float[4];
+        final float[] farPointWorld = new float[4];
+
+        multiplyMV(nearPointWorld, 0, invertedViewProjectionMatrix, 0, nearPointNdc, 0);
+        multiplyMV(farPointWorld, 0, invertedViewProjectionMatrix, 0, farPointNdc, 0);
+
+        divideByW(nearPointWorld);
+        divideByW(farPointWorld);
+
+        Point nearPointRay = new Point(nearPointWorld[0], nearPointWorld[1], nearPointWorld[2]);
+        Point farPointRay = new Point(farPointWorld[0], farPointWorld[1], farPointWorld[2]);
+
+        return new Ray(nearPointRay, Geometry.vectorBetween(nearPointRay, farPointRay));
+    }
+
+    private void divideByW(float[] vector) {
+        vector[0] /= vector[3];
+        vector[1] /= vector[3];
+        vector[2] /= vector[3];
+    }
+
     void handleTouchPress(float normalizedX, float normalizedY) {
-        Log.i(TAG, "handleTouchPress, " + normalizedX + ", " + normalizedY);
+
+        Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+
+        // Now test if this ray intersects with the mallet by creating a bounding sphere that
+        // wraps the mallet.
+        Geometry.Sphere malletBoundingSphere = new Geometry.Sphere(new Point(
+                blueMalletPosition.x,
+                blueMalletPosition.y,
+                blueMalletPosition.z),
+                mallet.height / 2f);
+
+        // If the ray intersects (if the user touched a part of the screen that intersects the
+        // mallet's bounding sphere), then set malletPressed to true.
+        malletPressed = Geometry.intersects(malletBoundingSphere, ray);
+
+        Log.i(TAG, "handleTouchPress, " + normalizedX + ", " + normalizedY + ", " + malletPressed);
     }
 
     void handleTouchDrag(float normalizedX, float normalizedY) {
--- app/src/main/java/net/ghosttrails/openglplayground/util/Geometry.java
+++ app/src/main/java/net/ghosttrails/openglplayground/util/Geometry.java
@@ -17,6 +17,13 @@ public class Geometry {
         public Point translateY(float distance) {
             return new Point(x, y + distance, z);
         }
+
+        public Point translate(Vector vector) {
+            return new Point(
+                    x + vector.x,
+                    y + vector.y,
+                    z + vector.z);
+        }
     }
 
     public static class Circle {
@@ -43,5 +50,71 @@ public class Geometry {
             this.radius = radius;
             this.height = height;
         }
+    }
+
+    public static class Ray {
+        public final Point point;
+        public final Vector vector;
+
+        public Ray(Point point, Vector vector) {
+            this.point = point;
+            this.vector = vector;
+        }
+    }
+
+    public static class Vector {
+        public final float x, y, z;
+
+        public Vector(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float length() {
+            return (float) Math.sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector crossProduct(Vector other) {
+            return new Vector(
+                    (y * other.z) - (z * other.y),
+                    (z * other.x) - (x * other.z),
+                    (x * other.y) - (y * other.x));
+        }
+    }
+
+    public static class Sphere {
+        public final Point center;
+        public final float radius;
+
+        public Sphere(Point center, float radius) {
+            this.center = center;
+            this.radius= radius;
+        }
+    }
+
+    public static Vector vectorBetween(Point from, Point to) {
+        return new Vector(to.x - from.x, to.y - from.y, to.z - from.z);
+    }
+
+    public static boolean intersects(Sphere sphere, Ray ray) {
+        return distanceBetween(sphere.center, ray) < sphere.radius;
+    }
+
+    public static float distanceBetween(Point point, Ray ray) {
+        Vector p1ToPoint = vectorBetween(ray.point, point);
+        Vector p2ToPoint = vectorBetween(ray.point.translate(ray.vector), point);
+
+        // The length of the cross product gives the area of an imaginary parallelogram having the
+        // two vectors as sides. A parallelogram can be thought of a consisting of two triangles,
+        // so this is the same as twice the area of the triangle defined by the two vectors.
+        float areaOfTriangleTimeTwo = p1ToPoint.crossProduct(p2ToPoint).length();
+        float lengthOfBase = ray.vector.length();
+
+        // The area of a triangle is also equal to (base * height) / 2. In other words, the height
+        // is equal to (area * 2) / base. The height of this triangle is the distance from the
+        // point to the ray.
+        float distanceFromPointToRay = areaOfTriangleTimeTwo / lengthOfBase;
+        return distanceFromPointToRay;
     }
 }
